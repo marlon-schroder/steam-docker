13af93616e9202507459a0ba67cff618e7ac8479
Marlon Schroder <marlon.schroder@gmail.com>
2020-01-25 18:36:24 -0300

    upgrade

--- run_chromium.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-xhost +steam
-
-docker run --name steam --rm \
-	-e DISPLAY=${DISPLAY} \
-	steam \
-	subl &
--- run_sublime.sh
+++ run_sublime.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-xhost +steam
+xhost +
 
 docker run --name steam --rm \
+	-v /dev/dri:/dev/dri \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v /dev/shm:/dev/shm \
+	-v /etc/machine-id:/etc/machine-id \
+	-v ${HOME}:/home/marlon \
 	-e DISPLAY=${DISPLAY} \
+	--privileged=true \
 	steam \
-	subl &
+	sh -c "export LANG=en_US.UTF-8 && export LC_CTYPE=en_US.UTF-8 && sudo /etc/init.d/dbus start && subl"
