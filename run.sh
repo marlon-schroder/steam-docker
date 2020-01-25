3fbfad5c7558b18bad7aeb5b7ae299c380077f8e
Marlon Schroder <marlon.schroder@gmail.com>
2020-01-25 00:38:39 -0300

    upgrade

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #[ ! -d /run/user/pulse ] && doas mkdir /run/user/pulse
 
-xhost +
+xhost +steam
 
 docker run --name steam --rm \
 	-v /dev/dri:/dev/dri \
@@ -10,14 +10,9 @@ docker run --name steam --rm \
 	-v /dev/shm:/dev/shm \
 	-v /run/user/pulse:/run/user/${UID}/pulse \
 	-v /etc/machine-id:/etc/machine-id \
-	-v ${HOME}/steam/.local/share:/home/steam/.local/share \
-	-v ${HOME}/steam/.steam:/home/steam/.steam \
 	-v ${HOME}/downloads:/tmp/Downloads \
-	--privileged=true \
 	-e DISPLAY=${DISPLAY} \
 	steam \
 	/etc/init.d/steam &
 
-#docker commit 12fa3f4b1afd steam:latest
-#sudo /etc/init.d/dbus start & /bin/bash
-#rm -rf ~/.local/share/Steam && rm -rf ~/.steam* && steam
+#--privileged=true \
